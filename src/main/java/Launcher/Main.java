f9322df312450f8206475df25c776f30d8781092
Vicente Silva <v.silva06@ufromail.cl>
2021-07-06 20:00:51 -0400

    Package Gui, Launcher Class Gui: Inicio, Principal
    Class launcher: Main

---
+++ src/main/java/Launcher/Main.java
@@ -0,0 +1,12 @@
+package Launcher;
+
+import GUI.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        Inicio inicio = new Inicio();
+    }
+
+}
