0b1363733b48c1f11fe4f4a987b6476400fe9eae
Vicente Silva <v.silva06@ufromail.cl>
2021-07-26 00:15:12 -0400

    Update

--- src/main/java/Launcher/Main.java
+++ src/main/java/Launcher/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         GestorVentanas gestorVentanas = new GestorVentanas();
         gestorVentanas.ventanaInicio();
-        //gestorVentanas.abrirListaCompra();
 
     }
 }
